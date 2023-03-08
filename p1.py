7b01d2ba63c8533626ce1d17b96c0e096d38daf9
7753788 <68631002+7753788@users.noreply.github.com>
2023-03-08 17:26:42 +0500

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,26 @@
+print("[1] Login")
+print("[2] Create File")
+print("[3] Remove Duplicate")
+print("[4] Cookie")
+print("[5] Group Join")
+
+a = int(input("Your Choice : "))
+
+
+if a == 1 :
+	print("You are login")
+if a == 2 :
+	print("[1] Simple Make File")
+	print("[2] Unlimited Make With 1 id")
+	print("[3] Go Back")
+	b =int(input("Your Choice :"))
+	
+if a == 3 :
+	print("you are duplicate")
+if a == 4 :
+	print("Get cookie")
+if a == 5 :
+	print("Group join")
+elif a >= 6 :
+	print("Wrong input")
+
